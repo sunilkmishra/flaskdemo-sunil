1ba6bc622747edebc758683b0b5eeeac4b49c5ca
sunilkmishra <150823925+sunilkmishra@users.noreply.github.com>
2023-11-19 21:22:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import the Flask module
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the application if this script is executed
+if __name__ == '__main__':
+    app.run(debug=True)
